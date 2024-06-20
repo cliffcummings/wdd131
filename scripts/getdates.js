257f42374362eb82f023d590930a50e73b835d93
cliffcummings <cliffc@sunburst-design.com>
2024-06-20 12:42:40 -0600

    Typo corrections and cleanup

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,6 +4,6 @@ let author = "Cliff Cummings";
 let place = "Utah, USA";
 
 // \u00A9 is the unicode for the copywrite symbol
-// backticks allow us to inster variables into the output, similar to what we did in our programming classes.
+// backticks allow us to insert variables into the output.
 document.getElementById("copywrite").innerHTML = `\u00A9 ${yr} | ${author} | ${place}`;
 document.getElementById("modified").innerHTML = `Last Modified: ${lastModified}`;
