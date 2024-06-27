ba827388ea9974dbda5fce0c775c1fde89244b64
cliffcummings <cliffc@sunburst-design.com>
2024-06-26 22:15:48 -0600

    Debugging sandbox activity commit

---
+++ sandbox/debugging.js
@@ -0,0 +1,18 @@
+const PI = 3.14;
+let radius = 3;
+let area = 0;
+// area = radius * radius * PI;
+// console.log("Area1: ", area);
+area = circleArea(3);
+console.log("Area1: ", area);
+
+// radius = 4;
+// area = radius * radius * PI;
+// console.log("Area2: ", area);
+area = circleArea(4);
+console.log("Area2: ", area);
+
+function circleArea(radius) {
+    const area = radius * PI;
+    return(area);
+}
