238099042058eddb9f0f9fc20e098761e229c234
cliffcummings <cliffc@sunburst-design.com>
2024-07-21 19:53:37 -0600

    Image size adjustment on Cliff page

--- project/scripts/home.js
+++ project/scripts/home.js
@@ -1,26 +1,27 @@
-//----------------------------------------------------------------------------
-// Code to help detect sindow size and which html and css files are begin used
-// https://www.tutorialrepublic.com/codelab.php?topic=faq&file=javascript-window-resize-event
-//----------------------------------------------------------------------------
-function displayWindowInfo(){
-    // Get width and height of the window excluding scrollbars
-    let w = Number(document.documentElement.clientWidth);
+// // Used for debugging
+// //----------------------------------------------------------------------------
+// // Code to help detect window size and which css file is being used
+// // https://www.tutorialrepublic.com/codelab.php?topic=faq&file=javascript-window-resize-event
+// //----------------------------------------------------------------------------
+// function displayWindowInfo(){
+//     // Get width and height of the window excluding scrollbars
+//     let w = Number(document.documentElement.clientWidth);
 
-    if (w > 400) {
-        document.getElementById("css-src").innerHTML = `<p id="css-src">Uses sunburst-large.css</p>`;
-        // console.log("Large size!");
-    } else {
-        document.getElementById("css-src").innerHTML = `<p id="css-src">Uses sunburst.css</p>`;
-        // console.log("Small size!");
-    };
+//     if (w > 700) {
+//         document.getElementById("css-src").innerHTML = `<p id="css-src">Uses sunburst-large.css</p>`;
+//         // console.log("Large size!");
+//     } else {
+//         document.getElementById("css-src").innerHTML = `<p id="css-src">Uses sunburst.css</p>`;
+//         // console.log("Small size!");
+//     };
         
-    // Display result inside a div element
-    document.getElementById("result").innerHTML = "Width: " + w;
-}
+//     // Display result inside a div element
+//     document.getElementById("result").innerHTML = "Width: " + w;
+// }
  
-// Attaching the event listener function to window's resize event
-window.addEventListener("resize", displayWindowInfo);
+// // Attaching the event listener function to window's resize event
+// window.addEventListener("resize", displayWindowInfo);
 
-// Calling the function for the first time
-displayWindowInfo();
-//----------------------------------------------------------------------------
+// // Calling the function for the first time
+// displayWindowInfo();
+// //----------------------------------------------------------------------------
