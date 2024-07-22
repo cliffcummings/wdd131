79d2fdeb28788ddd6de178119945efa7a64d2aa7
cliffcummings <cliffc@sunburst-design.com>
2024-07-21 20:51:45 -0600

    Performance - local files added

--- project/scripts/cliff.js
+++ project/scripts/cliff.js
@@ -9,8 +9,10 @@ function mobileAdjust() {
                                              alt="Cliff Cummings photograph" height="480" align="left">`;
         // console.log("Large size!");
     } else {
-        document.getElementById("image-src").innerHTML = `<img src="./images/photo_cliff_187_240.png" 
-                                             alt="Cliff Cummings photograph" height="240" align="left">`;
+        document.getElementById("image-src").innerHTML = `<img src="./images/photo_cliff_374_480.png" 
+        alt="Cliff Cummings photograph" height="240" align="left">`;
+        // document.getElementById("image-src").innerHTML = `<img src="./images/photo_cliff_187_240.png" 
+        //                                      alt="Cliff Cummings photograph" height="240" align="left">`;
         // console.log("Small size!");
     };
 }
